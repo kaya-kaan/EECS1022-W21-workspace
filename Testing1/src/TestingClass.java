1e5aa0dc93c17d7cb147f5b6a9c55a65fce59cf1
Kaan Kaya <70816118+kaankaya7@users.noreply.github.com>
2021-01-13 03:01:29 -0500

    Added two projects to make tests

---
+++ Testing1/src/TestingClass.java
@@ -0,0 +1,12 @@
+
+public class TestingClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 13;
+		String b = "January";
+		int c = 2021;
+		System.out.println("Today is " + a + b + c);
+	}
+
+}
