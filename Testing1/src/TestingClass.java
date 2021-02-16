5f704c61ba4081d44bf54d77812f4439c2b648ec
Kaan Kaya <70816118+kaankaya7@users.noreply.github.com>
2021-02-16 00:58:20 -0500

    Changes made in TestingClass

--- Testing1/src/TestingClass.java
+++ Testing1/src/TestingClass.java
@@ -3,10 +3,26 @@ public class TestingClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 13;
-		String b = "January";
-		int c = 2021;
-		System.out.println("Today is " + a + b + c);
+//		int a = 13;
+//		String b = "January";
+//		int c = 2021;
+//		System.out.println("Today is " + a + b + c);
+//		int i = 13;
+//		
+//		int j = 2;
+//		System.out.println((double) (i / j));
+//		
+//		int count = 0;
+//		for (int k = -49; k < 49 ; k++) {
+//			count++;
+//		}
+//		System.out.println(count);
+int i = 5;
+int j = 7;
+int k = 9;
+String s = "Hello ";
+System.out.println(s + i + j + k);
+System.out.println((s + i) + j + k);
+System.out.println(s + (i + j) + k);
 	}
-
 }
