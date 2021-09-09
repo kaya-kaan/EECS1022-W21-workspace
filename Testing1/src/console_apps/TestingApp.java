eac927f43ac4a6297ee302cb7947617e47c479b1
Kaan Kaya <70816118+kaankaya7@users.noreply.github.com>
2021-09-09 00:32:40 -0400

    Testing

--- Testing1/src/console_apps/TestingApp.java
+++ Testing1/src/console_apps/TestingApp.java
@@ -1,16 +1,22 @@
 package console_apps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+
 import model.Person;
 
 public class TestingApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		
-		System.out.println("Done");
 
-
+		}
+		
+	
+	
+		
 	}
+	
 
-}
